68cb19bc9a704a13f3ab96dcc7266dbe3eaf585e
ShreedharJoshi31 <shreedharjoshi03@gmail.com>
2024-09-22 22:30:07 +0530

    Deploy

--- react-frontend/src/api/api.ts
+++ react-frontend/src/api/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "jiraApiReducer",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://task-hive-j39y.onrender.com/",
+    baseUrl: "https://task-hive-j39y.onrender.com/api",
     // baseUrl: "http://localhost:5000/api/",
     credentials: "include",
   }),
